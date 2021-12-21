b7c3c40e51688d7a36a030d95db1a4788c816154
eakyel <e.akyel@outlook.com>
2021-12-21 15:17:51 +0300

    Create mobile menu that sliding

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 'use strict';
 
 const menuButton = document.querySelector('#menu-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileBlackBg = document.querySelector('#filter');
+
 const navToggle = () => {
   menuButton.classList.toggle('open');
+  mobileMenu.classList.toggle('hidden');
+  document.body.classList.toggle('no-scroll');
+  mobileBlackBg.classList.toggle('black-filter');
 };
 
 menuButton.addEventListener('click', navToggle);
